a81dc4370596b0ee4bc827c70952f9666eb0146b
Jean-Paul Sartre <armins.bagrats@gmail.com>
2017-09-06 02:38:54 +0300

    fix example formatting

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
-var country = "US";
-var state = "All";
+var country = 'US';
+var state = 'All';
 
-$("#state").change(function(){
-    state = $("#state").find(":selected").val();
-    $("#example").html("<code>&lt;div id='widget'&gt;&lt;/div&gt; \n &lt;script src='https://bulkestate.github.io/js/widget.js' country='" + country + "' state='" + state + "'&gt;&lt;/script&gt;");
-    $("#widget").append();
+$('#state').change(function () {
+  state = $('#state').find(':selected').val();
+  $('#example').html("<code>&lt;div id='widget'&gt;&lt;/div&gt; \n&lt;script src='https://bulkestate.github.io/js/widget.js' country='" + country + "' state='" + state + "'&gt;&lt;/script&gt;");
+  $('#widget').append();
 });
