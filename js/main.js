0e33138ece4c28d994797521099383e8e8512180
Jean-Paul Sartre <armins.bagrats@gmail.com>
2017-09-06 02:56:51 +0300

    update widget based on drop-down

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@ var state = 'All';
 function refresh_content () {
   state = $('#state').find(':selected').val();
   $('#example').html("<code>&lt;div id='widget'&gt;&lt;/div&gt; \n&lt;script src='https://bulkestate.github.io/js/widget.js' country='" + country + "' state='" + state + "'&gt;&lt;/script&gt;");
-  $('#widget').append();
+  $('#widget-wrapper').html("<script src='https://bulkestate.github.io' country='" + country + "' state='" + state + "'></script>");
 }
 
 $(document).ready(function () {
