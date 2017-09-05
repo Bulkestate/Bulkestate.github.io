4bbf35659f88c69953cf661e97f8e2ae0c26ca3c
Jean-Paul Sartre <armins.bagrats@gmail.com>
2017-09-06 02:46:58 +0300

    update example on load

--- js/main.js
+++ js/main.js
@@ -1,8 +1,16 @@
 var country = 'US';
 var state = 'All';
 
-$('#state').change(function () {
+function refresh_content () {
   state = $('#state').find(':selected').val();
   $('#example').html("<code>&lt;div id='widget'&gt;&lt;/div&gt; \n&lt;script src='https://bulkestate.github.io/js/widget.js' country='" + country + "' state='" + state + "'&gt;&lt;/script&gt;");
   $('#widget').append();
+}
+
+$(document).ready(function () {
+  refresh_content();
+});
+
+$('#state').change(function () {
+  refresh_content();
 });
