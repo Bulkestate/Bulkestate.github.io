b17a1658f0cbfe570e87e100b869049cdb37dfcd
Jean-Paul Sartre <armins.bagrats@gmail.com>
2017-09-06 12:08:22 +0300

    add id to script'

--- js/main.js
+++ js/main.js
@@ -3,8 +3,8 @@ var state = 'All';
 
 function refresh_content () {
   state = $('#state').find(':selected').val();
-  $('#example').html("<code>&lt;div id='widget'&gt;&lt;/div&gt; \n&lt;script src='https://bulkestate.github.io/js/widget.js' country='" + country + "' state='" + state + "'&gt;&lt;/script&gt;");
-  $('#widget-wrapper').html("<script src='https://bulkestate.github.io' country='" + country + "' state='" + state + "'></script>");
+  $('#example').html("<code>&lt;div id='widget'&gt;&lt;/div&gt; \n&lt;script id='widget-script' src='https://bulkestate.github.io/js/widget.js' country='" + country + "' state='" + state + "'&gt;&lt;/script&gt;");
+  $('#widget-script').replaceWith("<script id='widget-script' src='https://bulkestate.github.io' country='" + country + "' state='" + state + "'></script>");
 }
 
 $(document).ready(function () {
